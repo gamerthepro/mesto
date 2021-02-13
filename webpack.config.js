9a9b03079b3e2d458a36cb3a7a81c725988c39ba
Artem Polonnikov <gamer.thepro@yandex.ru>
2021-02-13 22:06:53 +0300

    webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,59 +1,57 @@
-const path = require('path'); 
-const HtmlWebpackPlugin = require('html-webpack-plugin'); 
-const { CleanWebpackPlugin } = require('clean-webpack-plugin'); 
-const MiniCssExtractPlugin = require('mini-css-extract-plugin'); 
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    entry: { main: './src/pages/index.js' },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js',
-                publicPath: ''
-    },
-    mode: 'development',
-    devServer: {
-        contentBase: path.resolve(__dirname, './dist'), 
-        compress: true, 
-        port: 8080, 
-        open: true 
+  entry: {main: './src/pages/index.js'},
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'main.js',
+    publicPath: ''
   },
-    module: {
-    rules: [ 
-      {
-        test: /\.js$/,
-        use: 'babel-loader',
-        exclude: '/node_modules/'
-      },
-      {
-        test: /\.js$/,
-        use: 'babel-loader',
-        exclude: '/node_modules/'
-     },
-     {
-      test: /\.(png|svg|jpg|jpeg|gif)$/, type: 'asset/resource', generator: {
-        filename: 'images/[name].[contenthash][ext]',
-      }
-    }, 
+
+  mode: 'development',
+  devServer: {
+    contentBase: path.resolve(__dirname, './dist'),
+    compress: true,
+    port: 8080,
+
+    open: true
+  },
+
+  module: {
+    rules: [
     {
-      test: /\.(woff|woff2|eot|ttf|otf)$/i, type: 'asset/resource', generator: {
-        filename: 'vendor/[name].[contenthash][ext]',
+      test: /\.js$/,
+      use: 'babel-loader',
+      exclude: '/node_modules'
+    },
+
+    {
+      test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+      type: 'asset/resource'
+    },
+npm i css-loader --save-dev
+    {
+    test: /\.css$/,
+    use: [MiniCssExtractPlugin.loader, {
+      loader: 'css-loader',
+      options: {
+        importLoaders: 1
       }
     },
-    {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, {
-          loader: 'css-loader',
-          options: {importLoaders: 1}
-        },
-        'postcss-loader'
+    'postcss-loader'
     ]
-    }]},
+    },
+    ]
+  },
 
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html'
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
     }),
-        new CleanWebpackPlugin(),
-        new MiniCssExtractPlugin(),
-  ],  
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin()
+  ]
 }
